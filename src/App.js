baacd43ff8bcbc6a210402a353f11f26732cd42f
Sanjog <sanjogstha7@gmail.com>
2018-10-03 09:37:23 +0300

    redux is configured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/home'
 
 class App extends Component {
     render() {
         return (
-            <h1>hello sanjog</h1>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class test extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is just for testing</p>
+            </div>
+        )
+    }
+}
+
+
+export default test
