aa6bcf410ab00b8f16323659f462662a712b55e7
Sanjog <sanjogstha7@gmail.com>
2018-10-12 19:05:12 +0300

    kielikursseja mock data added

---
+++ src/MOCKS/index.js
@@ -0,0 +1,56 @@
+export const languageCourses = [
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+    {
+        title: 'title',
+        content: 'this is content area',
+        startDate: 'start date',
+        endDate: 'end date',
+    },
+]
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,16 +1,47 @@
 import React, {Component} from 'react'
 import {Row, Col, Input, InputGroup, InputGroupAddon, Button,
-    Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap'
+    Card, CardImg, CardText, CardBody, CardSubtitle} from 'reactstrap'
 import {ReactComponent  as SearchIcon} from 'hel-icons/dist/shapes/search.svg'
-import Koro from '../koroSection'
 import sportIcon from '../../assets/icons/icon-sport@2x.png'
 import langIcon from '../../assets/icons/icon-lang@2x.png'
 import artIcon from '../../assets/icons/icon-art@2x.png'
 import techIcon from '../../assets/icons/icon-tech@2x.png'
+import {languageCourses} from '../../MOCKS'
 import './index.scss'
 
 class FrontPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            itemsToShow: 3,
+            expanded: false,
+        }
+    }
+
+    showMore = () => {
+        this.state.itemsToShow < languageCourses.length ? (
+            this.setState({
+                itemsToShow: this.state.itemsToShow + 3,
+                expanded: true,
+            })
+        ) : (
+            this.setState({itemsToShow: 3, expanded: false})
+        )                 
+    }
     render() {
+        const courses = languageCourses.slice(0, this.state.itemsToShow).map((course, i)=>(
+            <Col xs="4" key={i}>
+                <Card>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardSubtitle>{course.title}</CardSubtitle>
+                        <CardText>course.content</CardText>
+                        <CardText>course.startDate</CardText>
+                        <CardText>course.endDate</CardText>                                        
+                    </CardBody>
+                </Card>
+            </Col>
+        ))
         return (
             <div className="front-page">
                 <section>
@@ -20,8 +51,6 @@ class FrontPage extends Component {
                                 <div className="banner-content">
                                     <h1>Kurssihaku</h1>
                                     <p>Opi uutta ja verestä vanhoja taitoja ja tietoja. Tutustu helsinkiläiseen kurssitarjontaan.</p>
-                                    {/* <SearchIcon className="search-icon" />
-                                    <Input type="text" placeholder="Hae kursseja"/> */}
                                     <InputGroup>
                                         <InputGroupAddon addonType="prepend"><SearchIcon className="search-icon" /></InputGroupAddon>
                                         <Input type="text" placeholder="Hae kursseja"/>
@@ -34,7 +63,7 @@ class FrontPage extends Component {
                 <section>
                     <Row>
                         <Col xs="12">
-                            <div className="icon-container">
+                            <div className="icon-container test">
                                 <div className="front-icons">
                                     <figure>
                                         <img src={sportIcon} alt="sport" />
@@ -53,7 +82,11 @@ class FrontPage extends Component {
                                         <figcaption>TEKNIKKAA</figcaption>
                                     </figure>
                                 </div>
-                                <div className="front-button"><Button outline color="light">Ja paljon muuta</Button></div>
+                                <div className="front-button">
+                                    <Button outline color="light">                                        
+                                        Ja paljon muuta
+                                    </Button>
+                                </div>
                             </div>
                             
                         </Col>
@@ -61,39 +94,18 @@ class FrontPage extends Component {
                 </section>
                 <section>
                     <h2>Kielikursseja</h2>
-                    <Row className="language-course-container">                    
-                        <Col xs="4">    
-                            <Card>
-                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                                <CardBody>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
-                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                                        
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col xs="4">  
-                            <Card>
-                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                                <CardBody>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
-                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                                        
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col xs="4">  
-                            <Card>
-                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                                <CardBody>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
-                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                                        
-                                </CardBody>
-                            </Card>
-                        </Col>
+                    <Row className="language-course-container"> 
+                        {courses}
                     </Row>
-                    <div className="front-button"><Button outline color="primary">Lisää kielikursseja</Button></div>
+                    <div className="front-button">
+                        
+                        <Button
+                            outline
+                            onClick={this.showMore} 
+                            color="primary">
+                            {this.state.itemsToShow === languageCourses.length ? 'Show Less' : 'Lisää kielikursseja'}
+                        </Button>
+                    </div>
                 </section>
             </div>            
         )
