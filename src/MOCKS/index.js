d60f7276bcdf50bded02082b2335c7127cebe209
Sanjog <sanjogstha7@gmail.com>
2018-10-18 16:46:57 +0300

    jumbotron added

--- src/MOCKS/index.js
+++ src/MOCKS/index.js
@@ -111,3 +111,9 @@ export const otherCourses = [
         endDate: 'end date',
     },
 ]
+
+export const options = [
+    {value: 'chocolate', label: 'Chocolate'},
+    {value: 'strawberry', label: 'Strawberry'},
+    {value: 'vanilla', label: 'Vanilla'},
+];
--- src/components/search_courses/index.js
+++ src/components/search_courses/index.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react'
-import {Row, Col, Label, Input, Jumbotron} from 'reactstrap'
+import {Row, Col, Label, Input, Jumbotron, FormGroup} from 'reactstrap'
+import Select from 'react-select'
+import {options} from '../../MOCKS'
 import './index.scss'
 
 class SearchCourses extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedOption: null,
+        }
+    }
+
+    handleChange = (selectedOption) => {
+        this.setState({selectedOption});
+        console.log(`Option selected:`, selectedOption);
+    }
     render() {
         return (
             <div className="search-course-section">
@@ -12,14 +25,35 @@ class SearchCourses extends Component {
                             <Jumbotron>
                                 <h1>Selaa Kursseja</h1>
                                 <div className="search-inputs">
-                                    <div className="form-group">
+                                    <FormGroup>
                                         <Label>Hakuteksti</Label>
                                         <Input type="text"  placeholder="Hae kurssin nimestä tai kuvauksesta" className="form-control" />
+                                    </FormGroup>
+                                    <div>
+                                        <Label>Kurssin aihe</Label>
+                                        <Select
+                                            value={this.state.selectedOption}
+                                            onChange={this.handleChange}
+                                            options={options}
+                                            className="react-select"
+                                        />
                                     </div>
+                                    <div>
+                                        <Label>Järjestäjä</Label>
+                                        <Select
+                                            value={this.state.selectedOption}
+                                            onChange={this.handleChange}
+                                            options={options}
+                                            className="react-select"
+                                        />
+                                    </div>                                    
                                 </div>
                             </Jumbotron>
                         </Col>
                     </Row>
+                </section>
+                <section>
+                    hello
                 </section>
             </div>
         )
